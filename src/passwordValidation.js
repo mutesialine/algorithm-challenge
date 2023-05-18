9600b7d34de2fb8975a8d7bc68206799be4ba880
mutesialine <ktesialine@gmail.com>
2023-05-18 13:08:33 +0200

    refactor:password validation

--- src/passwordValidation.js
+++ src/passwordValidation.js
@@ -7,11 +7,10 @@ export const forbiddenPasswords = ["amG84h6yeQ", "mc9Q20pdjH", "jnT6Q2f8U5"];
  * @returns {boolean}
  */
 export default function isValidPassword(password = "") {
-
   if (forbiddenPasswords.includes(password)) {
     return false;
   }
-   // The following line ensures, that password is always a string, like the number 128 -> string "128"
+  // The following line ensures, that password is always a string, like the number 128 -> string "128"
   if (typeof password !== "string") password = String(password);
 
   // Check if the password length is equal to 10
@@ -27,30 +26,30 @@ export default function isValidPassword(password = "") {
   if (!/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])/.test(password)) {
     return false;
   }
-  // check if the password doesn't contain the sequence of ascending or descending  numembers
-  if (hasSequence(password))
-  return false
+  //check if a sequence is found in the password
+  if (hasSequence(password)) return false;
 
-  
   const setOfPassword = new Set([...password]);
   if (setOfPassword.size < 4) {
     return false;
   }
   return true;
 }
-
-
+// Checks if the password contains a sequence of numbers in ascending or descending order it return True if a sequence is found, otherwise false.
 function hasSequence(password) {
   for (let i = 0; i < password.length - 2; i++) {
-    if (
-      (Number(password[i]) === Number(password[i + 1]) - 1 &&
-        Number(password[i + 1]) === Number(password[i + 2]) - 1) || // Ascending sequence
-      (Number(password[i]) === Number(password[i + 1]) + 1 &&
-        Number(password[i + 1]) === Number(password[i + 2]) + 1)    // Descending sequence
-    ) {
-      return true; // Sequence found
+    // Ascending sequence
+    const isAscending =
+      Number(password[i]) === Number(password[i + 1]) - 1 &&
+      Number(password[i + 1]) === Number(password[i + 2]) - 1;
+    // Descending sequence
+    const isDecending =
+      Number(password[i]) === Number(password[i + 1]) + 1 &&
+      Number(password[i + 1]) === Number(password[i + 2]) + 1;
+    if (isAscending || isDecending) {
+      return true;
     }
   }
 
-  return false; // No sequence found
+  return false;
 }
