d6ddf075f8df39b4dc3edbaf786149a7073514dc
mutesialine <ktesialine@gmail.com>
2023-05-23 11:58:49 +0200

    refactor: password penalty

--- src/passwordPenalty.js
+++ src/passwordPenalty.js
@@ -6,20 +6,20 @@
  * @returns {number}
  */
 export default function penaltyPoints(password = "") {
-  // The following line ensures, that password is always a string, like the number 128 -> string "128"
   if (!password) {
     return 0;
   }
-  if (typeof password !== "string")
-    //convert password to a stirng if it not a string
-    password = String(password);
-  //match the  repeated sequences of alphanumeric characters
+  if (typeof password !== "string") password = String(password);
+  //Match the  repeated sequences of alphanumeric characters
   const sequences = password.match(/([a-zA-Z0-9])\1{1,}/g);
-  return sequences
-    ? sequences.reduce(
-        (penalty, sequence) =>
-          penalty + (sequence.length === 2 ? 1 : sequence.length >= 3 ? 2 : 0),
-        0
-      )
-    : 0;
+  if (!sequences) {
+    return 0;
+  } else {
+    // If sequences exist and are truthy
+    return sequences.reduce((penalty, sequence) => {
+      return (
+        penalty + (sequence.length === 2 ? 1 : sequence.length >= 3 ? 2 : 0)
+      );
+    }, 0);
+  }
 }
